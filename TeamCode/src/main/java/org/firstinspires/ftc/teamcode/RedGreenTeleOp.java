9f1532f1056660b3651cfa1cfbc25e9a5fe3fd07
Sofia <sathemudo@gmail.com>
2024-12-04 20:48:27 -0500

    First version of TeleOp and auto (for Red and Green Scrimmage)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedGreenTeleOp.java
@@ -0,0 +1,158 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Robot: RedGreenTeleOp", group="Robot")
+//@Disabled
+public class RedGreenTeleOp extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    public DcMotor  mFL  = null;
+    public DcMotor  mFR  = null;
+    public DcMotor  mBL  = null;
+    public DcMotor  mBR  = null;
+    public DcMotor  mArm = null;
+    public DcMotor  mLS  = null;
+    public Servo Intake  = null;
+
+    double clawOffset = 0;
+
+
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double drive;
+        double turn;
+        double max;
+
+        mFL  = hardwareMap.get(DcMotor.class, "mFL");
+        mFR  = hardwareMap.get(DcMotor.class, "mFR");
+        mBL  = hardwareMap.get(DcMotor.class, "mBL");
+        mBR  = hardwareMap.get(DcMotor.class, "mBR");
+        mArm = hardwareMap.get(DcMotor.class, "mArm");
+        mLS  = hardwareMap.get(DcMotor.class, "mLS");
+
+        mFL.setDirection(DcMotor.Direction.REVERSE);
+        mFR.setDirection(DcMotor.Direction.FORWARD);
+        mBL.setDirection(DcMotor.Direction.REVERSE);
+        mBR.setDirection(DcMotor.Direction.FORWARD);
+        mArm.setDirection(DcMotor.Direction.REVERSE);
+        mLS.setDirection(DcMotor.Direction.REVERSE);
+
+        mFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mLS.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //zero power
+
+        Intake  = hardwareMap.get(Servo.class, "Intake");
+        Intake.setPosition(0.5);
+
+        telemetry.addData(">", "Robot Ready.  Press START.");    //
+        telemetry.update();
+
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Drivetrain
+            drive = -gamepad1.left_stick_y;
+            turn  =  gamepad1.right_stick_x;
+
+            left  = drive + turn;
+            right = drive - turn;
+
+            max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0)
+            {
+                left /= max;
+                right /= max;
+            }
+
+            mFL.setPower(left);
+            mFR.setPower(right);
+            mBL.setPower(left);
+            mBR.setPower(right);
+
+
+            // Arm - up (Y), down (A)
+            if (gamepad1.y)
+                mArm.setPower(0.6);
+            else if (gamepad1.a)
+                mArm.setPower(-0.6);
+            else
+                mArm.setPower(0.0);
+
+
+            // Linear Slide - up (dpad up), down (dpad down)
+            if (gamepad1.dpad_up)
+                mLS.setPower(0.6);
+            else if (gamepad1.dpad_down)
+                mLS.setPower(-0.6);
+            else
+                mLS.setPower(0.0);
+
+
+            //Intake - open (), close ()left & right Bumpers to open and close the claw
+            if (gamepad1.right_bumper)
+                clawOffset += 0.2;
+            else if (gamepad1.left_bumper)
+                clawOffset -= 0.2;
+            else
+                clawOffset = 0;
+
+            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+            Intake.setPosition(0.5 + clawOffset);
+
+
+            // Send telemetry message to signify robot running;
+            telemetry.addData("Intake",  "Offset = %.2f", clawOffset);
+            telemetry.addData("left",  "%.2f", left);
+            telemetry.addData("right", "%.2f", right);
+            telemetry.update();
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+}
